bf53202f03c5fed7a7acffda66a21a6a39455b49
Kash Nouroozi <knouroozi@gmail.com>
2016-10-03 19:51:32 -0400

    Test core/pair and stub other fns

--- src/double_booked/core.clj
+++ src/double_booked/core.clj
@@ -1,7 +1,20 @@
 (ns double-booked.core
   (:require [clj-time.core :as t]))
 
-(defn overlapping-pairs [events]
-  (if-let [events (seq events)]
-    []
-    []))
+(defn overlap? [event-a event-b]
+  (t/overlaps? (:time event-a) (:time event-b)))
+
+(def start-time (comp t/start :time))
+
+(defn pair [event-a event-b]
+  (sort-by start-time t/before? [event-a event-b]))
+
+(defn pairs [events]
+  (loop [es events
+         ps []]
+    (if (seq es)
+      []
+      ps)))
+
+(defn sorted-pairs [events]
+  (sort-by (comp start-time first) t/before? (pairs events)))
--- test/double_booked/core_test.clj
+++ test/double_booked/core_test.clj
@@ -1,10 +1,31 @@
 (ns double-booked.core-test
   (:require [clojure.test :refer :all]
+            [clj-time.core :as t]
             [double-booked.test-helper :as helper]
             [double-booked.core :as d]))
 
 (def events (helper/stubbed-events 16))
 
+(deftest events-pair
+  (testing "A pair of events is sorted by start time"
+    (let [[event-a event-b] (take 2 (shuffle events))
+          actual (d/pair event-a event-b)]
+      (if (t/before? (d/start-time event-a) (d/start-time event-b))
+        (is (= [event-a event-b] actual))
+        (is (= [event-b event-a] actual))))))
+
 (deftest empty-events-seq
-  (testing "An empty events list should return an empty list"
-    (is (= (d/overlapping-pairs []) []))))
+  (testing "An empty events sequence should return an empty list"
+    (let [expected []
+          actual (d/pairs [])]
+      (is (= expected actual)))))
+
+;; (deftest two-events-seq
+;;   (testing "A seq of two overlapping or non-overlapping events should behave properly"
+;;     (let [event-a (rand-nth events)
+;;           event-b (rand-nth events)
+;;           expected (if (t/overlaps? (:time event-a) (:time event-b))
+;;                      [(d/pair event-a event-b)]
+;;                      [])]
+;;       (is (= expected (d/pairs [event-a event-b]))))))
+
