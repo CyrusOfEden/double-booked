3a12e6d2eacc9701b7b9a81adf8c143ae73277d2
Kash Nouroozi <knouroozi@gmail.com>
2016-10-03 20:24:52 -0400

    Prepare for exhaustive testing

--- test/double_booked/core_test.clj
+++ test/double_booked/core_test.clj
@@ -4,11 +4,9 @@
             [double-booked.test-helper :as helper]
             [double-booked.core :as d]))
 
-(def events (helper/stubbed-events 16))
-
 (deftest events-pair
   (testing "A pair of events is sorted by start time"
-    (let [[event-a event-b] (take 2 (shuffle events))
+    (let [[event-a event-b] (helper/stubbed-events 2)
           actual (d/pair event-a event-b)]
       (if (t/before? (d/start-time event-a) (d/start-time event-b))
         (is (= [event-a event-b] actual))
@@ -24,14 +22,8 @@
   (testing "Expect all pairs to overlap, don't expect any false positives"
     (letfn [(check [[event-a event-b]]
               (t/overlaps? (:time event-a) (:time event-b)))]
-      (is (every? check (d/pairs events))))))
+      (is (every? check (d/pairs (helper/stubbed-events 16)))))))
 
-;; (deftest two-events-seq
-;;   (testing "A seq of two overlapping or non-overlapping events should behave properly"
-;;     (let [event-a (rand-nth events)
-;;           event-b (rand-nth events)
-;;           expected (if (t/overlaps? (:time event-a) (:time event-b))
-;;                      [(d/pair event-a event-b)]
-;;                      [])]
-;;       (is (= expected (d/pairs [event-a event-b]))))))
+(deftest events-seq-exhaustive
+  (testing "Expect overlapping pairs function to return all overlaps"))
 
