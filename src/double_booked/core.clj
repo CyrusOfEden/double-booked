bd60997fb26d42b1cbde53cddfcf692a37cea321
Kash Nouroozi <knouroozi@gmail.com>
2016-10-04 14:55:19 -0400

    Reorganize fns (private first, public last) using declare

--- src/double_booked/core.clj
+++ src/double_booked/core.clj
@@ -1,12 +1,7 @@
 (ns double-booked.core
   (:require [clj-time.core :as t]))
 
-(defrecord Event [name start end])
-
-(defn overlap? [event-a event-b]
-  """Check whether or not two events overlap"""
-  (and (t/before? (:start event-a) (:end event-b))
-       (t/before? (:start event-b) (:end event-a))))
+(declare overlap? sort-pair overlaps-for overlapping-pairs)
 
 (defn- sort-pair [sort? map-key event]
   """Return a sorter for a given pair of events by key (:start or :end)"""
@@ -20,6 +15,13 @@
   """Pair an event with every other event in a sequence that overlaps with it"""
   (comp (take-while #(overlap? event %))
         (map (sort-pair sort-pairs sort-key event))))
+
+(defrecord Event [name start end])
+
+(defn overlap? [event-a event-b]
+  """Check whether or not two events overlap"""
+  (and (t/before? (:start event-a) (:end event-b))
+       (t/before? (:start event-b) (:end event-a))))
 
 (defn overlapping-pairs [events & {:keys [sort-pairs sort-key]
                                    :or [sort-pairs true
